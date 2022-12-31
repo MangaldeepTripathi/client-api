243c743970af2d17b2bcd97471fb72ed23de15ae
MD Tripathi <mangaldeeptripathi@gmail.com>
2023-01-01 02:44:21 +0530

    Added the jsont webtoken in redis database and also updated the refresh token in mongo

--- src/routers/user.router.js
+++ src/routers/user.router.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router();
 const { insertUser, getUserByEmail } = require("../model/user/User.model");
 const { hashPassword,comparePassword } = require("../helpers/bcrypt.helper")
+const{ createAccessJWT,createRefreshJWT}= require("../helpers/jwt.helper")
 router.all("/", (req, res, next) => {
     // console.log(name)
     //res.json({message:"return from user router"})
@@ -48,7 +49,14 @@ router.post("/login", async (req, res) => {
     if (!result) {
 		return res.json({ status: "error", message: "Invalid email or password!" });
 	}
-    console.log(user);
-    res.json({status:"success", message:"User loggedin successfully"}) 
+    const accessJWT= await createAccessJWT(user.email, `${user._id}`);
+    const refreshJWT= await createRefreshJWT(user.email, `${user._id}`);
+
+    res.json({
+        status:"success",
+        message:"User loggedin successfully",
+        accessJWT,
+        refreshJWT
+    }) 
 })
 module.exports = router;
